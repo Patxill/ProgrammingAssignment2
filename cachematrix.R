ad29fd33251964160b24ce8e70d9f6a8633fcfc4
Francisco <patxillg@gmail.com>
2015-08-24 00:16:15 +0100

    Makecachematrix function is ready

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,14 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This is a function that creates a matrix that can cache its inversion
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
+## This function will provide the inverse of the matrix calculating it only if it doesn't exist in the cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
